c567d044cbc950b2534fb830f8ae4c676bb0b6e8
Georgi Gospodinov <regjets@gmail.com>
2026-02-04 14:24:17 +0200

    morning 04.02

---
+++ ProgramingBasics/2025SoftUniSelfStudy/Checks/totalTime.js
@@ -0,0 +1,12 @@
+function Time(timeFirst, timeSecond, timeThird){
+    let totalTime = timeFirst + timeSecond + timeThird;
+    let minutes = Math.floor(totalTime / 60);
+    let seconds = totalTime % 60;
+    if (seconds < 10){
+        console.log(`${minutes}:0${seconds}`);
+
+    } else{
+        console.log(`${minutes}:${seconds}`);
+    }
+}
+Time(35,45,44);
