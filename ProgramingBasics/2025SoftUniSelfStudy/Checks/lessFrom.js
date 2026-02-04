d0fb2adf6ecf105e463a904df3713f84cbe37a93
Georgi Gospodinov <regjets@gmail.com>
2026-02-04 02:16:32 +0200

    Night study

---
+++ ProgramingBasics/2025SoftUniSelfStudy/Checks/lessFrom.js
@@ -0,0 +1,14 @@
+function numbers(position){
+    if (position < 100) {
+        console.log ('Less than 100')
+    } 
+    else if (position > 200){
+        console.log ('Grater than 200')
+    }
+    else {
+        console.log ('Between 100 and 200')
+    }
+} 
+    
+      
+numbers(1);
---
+++ ProgramingBasics/2025SoftUniSelfStudy/Checks/password.js
@@ -0,0 +1,8 @@
+function guessPassword(password){
+    if (password === 's3cr3t!Passw0rd'){
+        console.log('Welcome');
+    } else {
+        console.log('Wrong password!');
+    }
+}
+guessPassword('s3cr3t!Passw0rd');
